90096fb673f65c771cb160c6eab0bd2511abc809
Manjeet <Manjeet@Manjeet-PC>
2018-06-11 10:13:31 +0530

    push by Manjeet tl

---
+++ TestProgram/src/com/sub/test/MyProgram.java
@@ -0,0 +1,11 @@
+package com.sub.test;
+
+public class MyProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Manjeet  kuamar");
+
+	}
+
+}
