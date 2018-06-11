6dd1f4a0ec5e8ea77a47b941a11fcec08b15f420
Manjeet <Manjeet@Manjeet-PC>
2018-06-11 12:22:45 +0530

    Manjeet-TL updated

--- ProgTestSample/src/com/sub/test/MyTest.java
+++ ProgTestSample/src/com/sub/test/MyTest.java
@@ -5,8 +5,11 @@ public class MyTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println(" Kumar thakur Manjeet  ---Malhotra");
-    
+   
     System.out.println("good-Afternoon");
+    
+    OddEvenProg find=new OddEvenProg();
+    System.out.println(find.findOddEven(9));
     
 	}
 
---
+++ ProgTestSample/src/com/sub/test/OddEvenProg.java
@@ -0,0 +1,14 @@
+package com.sub.test;
+
+public class OddEvenProg {
+
+	
+	public String findOddEven(int num){
+		
+		
+		if(num%2==0)
+			return "Even";
+		else
+			return "odd";
+	}
+}
